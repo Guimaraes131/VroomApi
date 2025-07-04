d7c1125bc0e5f27ec24a8cd0b3bffcacc795b7fd
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-04 14:54:49 -0300

    Starting project

---
+++ src/main/java/io/github/Guimaraes131/VroomApi/VroomApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.VroomApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VroomApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VroomApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/Guimaraes131/VroomApi/VroomApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Guimaraes131.VroomApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VroomApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
