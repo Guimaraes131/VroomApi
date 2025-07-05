65b6ed20071cafc73991843ab34e38fb4db21d8e
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-05 08:08:38 -0300

    Validating business rules related to Motorcycle DELETE and CREATE methods

--- src/main/java/io/github/Guimaraes131/VroomApi/service/MotorcycleService.java
+++ src/main/java/io/github/Guimaraes131/VroomApi/service/MotorcycleService.java
@@ -3,6 +3,7 @@ package io.github.Guimaraes131.VroomApi.service;
 import io.github.Guimaraes131.VroomApi.model.Motorcycle;
 import io.github.Guimaraes131.VroomApi.model.enums.ProblemCategory;
 import io.github.Guimaraes131.VroomApi.repository.MotorcycleRepository;
+import io.github.Guimaraes131.VroomApi.validator.MotorcycleValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -17,8 +18,14 @@ import java.util.UUID;
 public class MotorcycleService {
 
     private final MotorcycleRepository repository;
+    private final MotorcycleValidator validator;
 
     public void create(Motorcycle motorcycle) {
+        validator.validate(motorcycle);
+
+        motorcycle.getTag().setIsAvailable(false);
+        motorcycle.getTag().setColor(motorcycle.getProblem().getAssociatedColor());
+
         repository.save(motorcycle);
     }
 
@@ -27,6 +34,7 @@ public class MotorcycleService {
     }
 
     public void delete(Motorcycle motorcycle) {
+        motorcycle.getTag().setIsAvailable(true);
         repository.delete(motorcycle);
     }
 
---
+++ src/main/java/io/github/Guimaraes131/VroomApi/validator/MotorcycleValidator.java
@@ -0,0 +1,24 @@
+package io.github.Guimaraes131.VroomApi.validator;
+
+import io.github.Guimaraes131.VroomApi.exception.InvalidOperationException;
+import io.github.Guimaraes131.VroomApi.model.Motorcycle;
+import io.github.Guimaraes131.VroomApi.repository.MotorcycleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MotorcycleValidator {
+
+    private MotorcycleRepository repository;
+
+    public void validate(Motorcycle motorcycle) {
+        if (tagIsNotAvailable(motorcycle)) {
+            throw new InvalidOperationException("Cannot create a motorcycle with a tag that is being used.");
+        }
+    }
+
+    private boolean tagIsNotAvailable(Motorcycle motorcycle) {
+        return !motorcycle.getTag().getIsAvailable();
+    }
+}
