1e4759f9fc916187798b24991f8893c88e35ad10
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-06 05:51:26 -0300

    Add color field validation in PostTagDTO to recognize the right pattern of RGB and size

--- src/main/java/io/github/Guimaraes131/VroomApi/controller/dto/PostTagDTO.java
+++ src/main/java/io/github/Guimaraes131/VroomApi/controller/dto/PostTagDTO.java
@@ -1,13 +1,15 @@
 package io.github.Guimaraes131.VroomApi.controller.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public record PostTagDTO(
         @NotBlank(message = "field cannot be null or blank")
         @Size(min = 3, max = 3, message = "field needs to have 3 characters")
         String coordinate,
+
+        @Pattern(regexp = "^RGB\\(\\s*([0-9]{1,3})\\s*,\\s*([0-9]{1,3})\\s*,\\s*([0-9]{1,3})\\s*\\)$",
+                message = "Invalid format.")
+        @Size(min = 13, max = 18, message = "field needs to have between 13 to 18 characters.")
         String color,
 
         @NotNull(message = "field cannot be null")
