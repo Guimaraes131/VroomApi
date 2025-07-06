d60752ae763fe44ce9d8677e7ef627476ae71b4f
Guilherme <guilhermegpereira26@hotmail.com>
2025-07-06 06:32:53 -0300

    Add duplicated record validation in MotorcycleValidator

--- src/main/java/io/github/Guimaraes131/VroomApi/validator/MotorcycleValidator.java
+++ src/main/java/io/github/Guimaraes131/VroomApi/validator/MotorcycleValidator.java
@@ -1,24 +1,43 @@
 package io.github.Guimaraes131.VroomApi.validator;
 
+import io.github.Guimaraes131.VroomApi.exception.DuplicatedRecordException;
 import io.github.Guimaraes131.VroomApi.exception.InvalidOperationException;
 import io.github.Guimaraes131.VroomApi.model.Motorcycle;
 import io.github.Guimaraes131.VroomApi.repository.MotorcycleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 public class MotorcycleValidator {
 
-    private MotorcycleRepository repository;
+    private final MotorcycleRepository repository;
 
     public void validate(Motorcycle motorcycle) {
-        if (tagIsNotAvailable(motorcycle)) {
+        if (existsMotorcycle(motorcycle)) {
+            throw new DuplicatedRecordException("Cannot proceed: data is already in use by another record.");
+        }
+
+        if (!tagIsAvailable(motorcycle)) {
             throw new InvalidOperationException("Cannot create or update a motorcycle with a tag that is being used.");
         }
     }
 
-    private boolean tagIsNotAvailable(Motorcycle motorcycle) {
-        return !motorcycle.getTag().getIsAvailable();
+    private boolean tagIsAvailable(Motorcycle motorcycle) {
+        return motorcycle.getTag().getIsAvailable();
+    }
+
+    private boolean existsMotorcycle(Motorcycle motorcycle) {
+        Optional<Motorcycle> optionalMotorcycle = repository.findByLicensePlateOrChassis(
+                motorcycle.getLicensePlate(), motorcycle.getChassis()
+        );
+
+        if (motorcycle.getId() == null) {
+            return optionalMotorcycle.isPresent();
+        }
+
+        return optionalMotorcycle.isPresent() && !motorcycle.getId().equals(optionalMotorcycle.get().getId());
     }
 }
